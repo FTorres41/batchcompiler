5c8d0c221728b5ec1397e862aacad9fb83ac996d
FTorres41 <fabio.torres.41@gmail.com>
2017-03-17 10:43:11 -0300

    Add files via upload

---
+++ PersonalAppCompilator3000Tabajara/PersonalAppCompilator3000Tabajara/PAC3K.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace PersonalAppCompilator3000Tabajara
+{
+    partial class PAC3K
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PAC3K));
+            this.quantityLbl = new System.Windows.Forms.Label();
+            this.quantityCmbBox = new System.Windows.Forms.ComboBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.folderTxtBox = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // quantityLbl
+            // 
+            this.quantityLbl.AutoSize = true;
+            this.quantityLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityLbl.Location = new System.Drawing.Point(10, 15);
+            this.quantityLbl.Name = "quantityLbl";
+            this.quantityLbl.Size = new System.Drawing.Size(207, 13);
+            this.quantityLbl.TabIndex = 2;
+            this.quantityLbl.Text = "Quantos arquivos deseja executar?";
+            // 
+            // quantityCmbBox
+            // 
+            this.quantityCmbBox.FormattingEnabled = true;
+            this.quantityCmbBox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.quantityCmbBox.Location = new System.Drawing.Point(12, 31);
+            this.quantityCmbBox.Name = "quantityCmbBox";
+            this.quantityCmbBox.Size = new System.Drawing.Size(205, 21);
+            this.quantityCmbBox.TabIndex = 3;
+            this.quantityCmbBox.SelectedIndexChanged += new System.EventHandler(this.quantityCmbBox_SelectedIndexChanged);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(6, 25);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(16, 13);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "1.";
+            this.lbl1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(30, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(421, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Visible = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(30, 49);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(421, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.Visible = false;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(8, 106);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(16, 13);
+            this.lbl4.TabIndex = 6;
+            this.lbl4.Text = "4.";
+            this.lbl4.Visible = false;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(30, 76);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(421, 21);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.Visible = false;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(8, 79);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(16, 13);
+            this.lbl3.TabIndex = 8;
+            this.lbl3.Text = "3.";
+            this.lbl3.Visible = false;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(30, 103);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(421, 21);
+            this.comboBox4.TabIndex = 11;
+            this.comboBox4.Visible = false;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(8, 52);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(16, 13);
+            this.lbl2.TabIndex = 10;
+            this.lbl2.Text = "2.";
+            this.lbl2.Visible = false;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(30, 130);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(421, 21);
+            this.comboBox5.TabIndex = 13;
+            this.comboBox5.Visible = false;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(8, 133);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(16, 13);
+            this.lbl5.TabIndex = 12;
+            this.lbl5.Text = "5.";
+            this.lbl5.Visible = false;
+            // 
+            // btnRun
+            // 
+            this.btnRun.FlatAppearance.BorderSize = 0;
+            this.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRun.Image = global::PersonalAppCompilator3000Tabajara.Properties.Resources._1464208942_Play01;
+            this.btnRun.Location = new System.Drawing.Point(391, 157);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(60, 26);
+            this.btnRun.TabIndex = 24;
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFolder.Location = new System.Drawing.Point(223, 15);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(189, 13);
+            this.lblFolder.TabIndex = 25;
+            this.lblFolder.Text = "Qual pasta contém os arquivos?";
+            // 
+            // folderTxtBox
+            // 
+            this.folderTxtBox.Location = new System.Drawing.Point(226, 32);
+            this.folderTxtBox.Name = "folderTxtBox";
+            this.folderTxtBox.Size = new System.Drawing.Size(201, 20);
+            this.folderTxtBox.TabIndex = 26;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackgroundImage = global::PersonalAppCompilator3000Tabajara.Properties.Resources.search;
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Location = new System.Drawing.Point(433, 29);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(36, 23);
+            this.btnSearch.TabIndex = 27;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRun);
+            this.groupBox1.Controls.Add(this.lbl1);
+            this.groupBox1.Controls.Add(this.lbl5);
+            this.groupBox1.Controls.Add(this.comboBox5);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.lbl4);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.lbl2);
+            this.groupBox1.Controls.Add(this.lbl3);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 190);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Batches a serem executados";
+            // 
+            // PAC3K
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 264);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.folderTxtBox);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.quantityCmbBox);
+            this.Controls.Add(this.quantityLbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PAC3K";
+            this.Text = "Personal App Compilator 3000 Tabajara";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label quantityLbl;
+        private System.Windows.Forms.ComboBox quantityCmbBox;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lblFolder;
+        private System.Windows.Forms.TextBox folderTxtBox;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ PersonalAppCompilator3000Tabajara/PersonalAppCompilator3000Tabajara/PAC3K.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalAppCompilator3000Tabajara
+{
+    public partial class PAC3K : Form
+    {
+        #region Atributos
+
+        public string folderPath;
+        public string[] batFiles;
+        public string[] vbsFiles;
+        public List<string> batchList;
+
+        #endregion
+
+        public PAC3K()
+        {
+            InitializeComponent();
+        }
+
+        #region Funções
+        private void runBatch(string path)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(path);
+
+            psi.UseShellExecute = false;
+
+            Process proc = new Process();
+            proc.StartInfo = psi;
+
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private List<string> getBatches()
+        {
+            List<string> batchList = new List<string>();
+
+            if (comboBox1.SelectedItem != null)
+            {
+                batchList.Add(comboBox1.SelectedItem.ToString());
+            }
+
+            if (comboBox2.SelectedItem != null)
+            {
+                batchList.Add(comboBox2.SelectedItem.ToString());
+            }
+
+            if (comboBox3.SelectedItem != null)
+            {
+                batchList.Add(comboBox3.SelectedItem.ToString());
+            }
+
+            if (comboBox4.SelectedItem != null)
+            {
+                batchList.Add(comboBox4.SelectedItem.ToString());
+            }
+
+            if (comboBox5.SelectedItem != null)
+            {
+                batchList.Add(comboBox5.SelectedItem.ToString());
+            }
+
+            return batchList;
+        }
+
+        #endregion
+
+        private void quantityCmbBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (quantityCmbBox.SelectedItem.ToString() == "1")
+            {
+                lbl1.Visible = true;
+                comboBox1.Visible = true;
+            }
+            else if (quantityCmbBox.SelectedItem.ToString() == "2")
+            {
+                lbl1.Visible = true;
+                comboBox1.Visible = true;
+                lbl2.Visible = true;
+                comboBox2.Visible = true;
+            }
+            else if (quantityCmbBox.SelectedItem.ToString() == "3")
+            {
+                lbl1.Visible = true;
+                comboBox1.Visible = true;
+                lbl2.Visible = true;
+                comboBox2.Visible = true;
+                lbl3.Visible = true;
+                comboBox3.Visible = true;
+            }
+            else if (quantityCmbBox.SelectedItem.ToString() == "4")
+            {
+                lbl1.Visible = true;
+                comboBox1.Visible = true;
+                lbl2.Visible = true;
+                comboBox2.Visible = true;
+                lbl3.Visible = true;
+                comboBox3.Visible = true;
+                lbl4.Visible = true;
+                comboBox4.Visible = true;
+            }
+            else if (quantityCmbBox.SelectedItem.ToString() == "5")
+            {
+                lbl1.Visible = true;
+                comboBox1.Visible = true;
+                lbl2.Visible = true;
+                comboBox2.Visible = true;
+                lbl3.Visible = true;
+                comboBox3.Visible = true;
+                lbl4.Visible = true;
+                comboBox4.Visible = true;
+                lbl5.Visible = true;
+                comboBox5.Visible = true;
+            }
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            batchList = getBatches();
+
+            foreach(var batch in batchList)
+            {
+                runBatch(batch);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            folderPath = folderTxtBox.Text;
+
+            if (quantityCmbBox.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, selecione uma quantidade de arquivos a serem rodados", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (folderPath == string.Empty)
+            {
+                MessageBox.Show("Por favor, insira uma pasta válida", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                folderTxtBox.Focus();
+                return;
+            }
+            else if (!folderPath.Contains("\\"))
+            {
+                if (MessageBox.Show("Por favor, insira uma pasta válida", "Erro", MessageBoxButtons.YesNo, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1).Equals(DialogResult.Yes))
+                {
+                    folderTxtBox.Text = "";
+                    folderTxtBox.Clear();
+                    folderTxtBox.Focus();
+                    return;
+                }
+            }
+
+            batFiles = Directory.GetFiles(folderPath, "*.bat");
+            vbsFiles = Directory.GetFiles(folderPath, "*.vbs");
+
+            comboBox1.Items.AddRange(batFiles);
+            comboBox2.Items.AddRange(batFiles);
+            comboBox3.Items.AddRange(batFiles);
+            comboBox4.Items.AddRange(batFiles);
+            comboBox5.Items.AddRange(batFiles);
+
+            comboBox1.Items.AddRange(vbsFiles);
+            comboBox2.Items.AddRange(vbsFiles);
+            comboBox3.Items.AddRange(vbsFiles);
+            comboBox4.Items.AddRange(vbsFiles);
+            comboBox5.Items.AddRange(vbsFiles);
+
+            MessageBox.Show("Arquivos encontrados!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+        }
+    }
+}
